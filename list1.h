d027eeddd5c2baf26161ed93d512fba45a690ade
HyunbinLee-hub <53569820+HyunbinLee-hub@users.noreply.github.com>
2020-06-22 09:55:16 +0900

    Add files via upload
    자료구조 연결리스트 과제 1번

--- list1.h
+++ list1.h
@@ -1,27 +1,26 @@
 #pragma once
 
-class LinkedList
+class LinkedList 
 {
 public:
 	LinkedList() {
-		head = NULL;	// 노드 포인터 head의 값 초기화 
+		head = NULL;		// 노드 포인터 head를 NULL로 초기화
 	}
-	~LinkedList() {		// 각 노드에 할당된 메모리 해제
+	~LinkedList() {
 		Node *temp = head;
 		Node *next_temp;
 
-		while (temp != NULL) {
+		while (temp != NULL) {	// 노드에 할당된 메모리 해제
 			next_temp = temp->next;
 			delete temp;
 			temp = next_temp;
 		}
 	}
 
-	void Append(int data);	// List의 마지막에 새로운 data에 대한 Node 추가
-	void Prepend(int data);
-	int Length();			// List에 있는 Node의 수를 return
-	void Print();			// List에 있는 모든 Node를 head부터 순서대로 출력
-
+	void Append(int data);		// List의 마지막에 새로운 data에 대한 Node 추가
+	int Length(void);			// List에 있는 Node의 수를 return
+	void Print(void);			// List에 있는 모든 Node를 head부터 순서대로 출력
+	
 
 private:
 	class Node
