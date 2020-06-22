8019d2b46056dbfba2a2d3b4a99f104709fe533d
HyunbinLee-hub <53569820+HyunbinLee-hub@users.noreply.github.com>
2020-06-22 09:58:29 +0900

    Add files via upload
    자료구조 연결리스트 구현 과제2

--- list2.h
+++ list2.h
@@ -3,9 +3,8 @@
 class LinkedList
 {
 public:
-	LinkedList() {		// 노드 포인터 head, tail의 값 초기화
-		head = NULL;
-		tail = NULL;
+	LinkedList() {
+		head = NULL;	// 노드 포인터 head의 값 초기화 
 	}
 	~LinkedList() {		// 각 노드에 할당된 메모리 해제
 		Node *temp = head;
@@ -20,23 +19,17 @@ public:
 
 	void Append(int data);	// List의 마지막에 새로운 data에 대한 Node 추가
 	void Prepend(int data);
-	int Length(void);			// List에 있는 Node의 수를 return
-	void Print(void);			// List에 있는 모든 Node를 head부터 순서대로 출력
-	void PrintReverse(void);
+	int Length();			// List에 있는 Node의 수를 return
+	void Print();			// List에 있는 모든 Node를 head부터 순서대로 출력
+
 
 private:
 	class Node
 	{
 		int data;		// Node에 저장될 data
 		Node *next;		// 다음 Node에 대한 pointer
-		Node *prev;
 
-		Node() {		// 노드 포인터 next, prev 초기화
-			prev = NULL;
-			next = NULL;
-		}
-		friend class LinkedList; 
+		friend class LinkedList;
 	};
 	Node *head;		// List의 가장 첫 Node를 가리키는 pointer
-	Node *tail;
 };
